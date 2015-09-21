75fb5ae96b349fde4bc0b98eb1cff9b02f18037d
Seyar Chapuh <seyar@yandex-team.ru>
2015-09-21 23:13:44 +0300

    save type to url

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ var DEFAULT_PARAMS = {lat: 44.68831017, lon: 34.4029205, zoom: 10};
 function App() {
     this._map = this._initMap();
     this._map.events.add('boundschange', this._saveLocation.bind(this));
+    this._map.events.add('typechange', this._saveLocation.bind(this));
 
     var typeSelector = this._map.controls.get('typeSelector');
     typeSelector.addMapType(this._addLayer('east-crimea/%z/tile-%x-%y.jpg', 'atlas'), 26);
